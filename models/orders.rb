510ca59f6d8173f073b0c28ec6398ed49049c004
nagane <naganeys@gmail.com>
2015-12-03 21:47:45 +0900

    path variable modify scope

--- models/orders.rb
+++ models/orders.rb
@@ -2,23 +2,24 @@ require 'pathname'
 require 'securerandom'
 
 class Orders
+  @path = Pathname.new("./data")
 
   def self.create(title,element)
 
-    path = Pathname.new("./data")
     title_id = SecureRandom.hex
-    File.open(path + title_id , "w") do |file|
+    File.open(@path + title_id , "w") do |file|
       file.puts element
     end
 
-    File.open(path + "orders-title-hash.txt","a") do |file|
+    File.open(@path + "orders-title-hash.txt","a") do |file|
       file.puts title_id + "," + title
     end
 
   end
 
   def self.get_order_titles
-
+    #todo topページにorder要素があればリンクを作りたい
+    File.read(@path + "orders-title-hash.txt")
   end
 
   def delete
